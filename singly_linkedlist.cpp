cc17023d0b6607601aaa11fbc39f9b38f69d6ae5
envikaaa <envikapriyatna31@gmail.com>
2026-04-24 15:22:21 +0700

    menambahkan if

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -23,5 +23,11 @@ public:
         int nim;
         cout << "\nMasukkan Nomor Mahasiswa: ";
         cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        
     }
 }
