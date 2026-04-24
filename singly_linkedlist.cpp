354e94bf582604d639aeff3899791a2a3374cfe5
envikaaa <envikapriyatna31@gmail.com>
2026-04-24 15:25:31 +0700

    nodeBaru->next = START;

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -34,6 +34,9 @@ public:
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
         }
     }
 }
